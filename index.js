03ee184e58774167d7904aef98872d638fdb9820
shubhamiu <patilsr@iu.edu>
2024-04-16 01:52:07 -0400

    seen added

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const io = require("socket.io")(8900, {
   },
 });
 
+const axios = require("axios")
+
 let users = [];
+ 
+
+
+
 
 const addUser = (userId, socketId) => {
   !users.some((user) => user.userId === userId) &&
@@ -29,8 +35,11 @@ io.on("connection", (socket) => {
     io.emit("getUsers", users);
   });
 
+  
+
   //send and get message
 socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+  
   const user = getUser(receiverId);
   if (user && user.socketId) {
     // Send message to receiver if they are online and socketId is known
@@ -43,6 +52,35 @@ socket.on("sendMessage", ({ senderId, receiverId, text }) => {
     console.log(`User with id ${receiverId} is not online.`);
   }
 });
+
+// seen
+
+socket.on("markMessagesAsSeen", async ({ conversationId, receiver_Id, seen }) => {
+  try {
+      
+    const res = await axios.put(
+      `http://localhost:8800/api/messages/`, // Assuming you need a specific endpoint
+      { conversationId , seen}
+    );
+    
+    console.log("SEENER:", receiver_Id)
+    const user = getUser(receiver_Id); // Correctly find the user to emit the event
+    if(user){
+    io.to(user.socketId).emit("messagesSeen", { conversationId });
+    }
+
+  } catch (err) {
+    console.error(err);
+  }
+});
+		// try {
+		// 	await Message.updateMany({ conversationId: conversationId, seen: false }, { $set: { seen: true } });
+		// 	await Conversation.updateOne({ _id: conversationId }, { $set: { "lastMessage.seen": true } });
+		// 	io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId });
+		// } catch (error) {
+		// 	console.log(error);
+		// }
+
 
   //when disconnect
   socket.on("disconnect", () => {
